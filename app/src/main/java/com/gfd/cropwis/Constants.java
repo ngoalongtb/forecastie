4895a4c65febc6bfc2242c34db2849ea8053d7de
Luan Ta <luantm@Luans-MacBook-Air.local>
2020-01-22 09:52:53 +0700

    change api from server to openweather

--- app/src/main/java/com/gfd/cropwis/Constants.java
+++ app/src/main/java/com/gfd/cropwis/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
     public static final int DEFAULT_ZOOM_LEVEL = 7;
 
     public static final String HOTSPOT_API = "http://cropinformationsystem.net/ug/service/hotspot?year=%d&week=%d";
-    public static final String NEW_HOTSPOT = "http://cropwis.org/ug/service/newhotspot";
-    public static final String FORECAST_16_DAYS = "http://cropwis.org/ug/service/openweather?lat=%s&lng=%s";
+    public static final String NEW_HOTSPOT = "https://cropwis.org/ug/service/newhotspot";
+    public static final String FORECAST_16_DAYS = "https://api.openweathermap.org/data/2.5/forecast/daily?&cnt=16&appid=974a4736057da52c4c98d7ea2b736f60&units=metric&lat=%s&lon=%s";
     public static final String FORECAST_16_DAYS_KEY = "FORECAST_16_DAYS_KEY";
     public static final String CROP_GROWING_SEASON_API = "http://cropwis.org/ug/service/ssinfo?lat=%s&lng=%s&type=%s";
 }
